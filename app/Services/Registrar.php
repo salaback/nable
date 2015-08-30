254f95d0c3a523694265929e9389dafe0809837a
Sean Alaback <sean.alaback@gmail.com>
2015-08-30 15:04:49 -0400

    create user table

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -31,8 +31,8 @@ class Registrar implements RegistrarContract {
 	public function create(array $data)
 	{
 		return User::create([
-			'first_name' => $data['name'],
-			'last_name' => $data['name'],
+			'first_name' => $data['first_name'],
+			'last_name' => $data['last_name'],
 			'email' => $data['email'],
 			'password' => bcrypt($data['password']),
 		]);
