8949e770267efaabea289b5edadfa8a043cf53b8
Sean Alaback <sean.alaback@gmail.com>
2015-09-13 16:44:30 -0400

    finish models and views of core site.

--- resources/views/includes/question.blade.php
+++ resources/views/includes/question.blade.php
@@ -1,7 +1,7 @@
-<div  class="list-group-item">
+<div  class="list-group-item" id="question-{{$question->id}}">
     <div class="question">
         {{$question->name}} <small>[{{$question->type}}]</small>
-        @if($question->defined == true)<button class="btn btn-xs btn-default pull-right"  data-toggle="modal" data-target="#addResponse" data-question="{{$question->id}}"><i class="glyphicon glyphicon-plus"></i> Add Response</button> @endif
+        <span class="pull-right"> @if($question->defined == true)<button class="btn btn-xs btn-default"  data-toggle="modal" data-target="#addResponse" data-question="{{$question->id}}"><i class="glyphicon glyphicon-plus"></i> Add Response</button> @endif <i class="btn btn-xs btn-danger glyphicon glyphicon-trash" id="delete-{{$question->id}}" onClick="deleteQuestion({{$question->id}})"></i> </span>
     </div>
     <div id="q-response-{{$question->id}}">
         @if($question->responses()->count() > 0)
