79a69486caab74ee7154b71d8fcfdf0c21e73631
Sean Alaback <sean.alaback@gmail.com>
2015-09-13 16:45:07 -0400

    added several methods related to replies

--- app/Modules/SMSQuery/RouteController.php
+++ app/Modules/SMSQuery/RouteController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\DB;
 
 class RouteController extends Controller {
 
+
     public function getAddToot(Request $request)
     {
         try {
@@ -26,20 +27,31 @@ class RouteController extends Controller {
         }
         finally
         {
-            Return redirect('/toot/smsquery/settings/' . $ti->id);
+            Return redirect('/toot/smsquery/settings?toot_id=' . $ti->id);
         }
 
     }
 
-    public function getSettings($id)
-    {   $toot['id'] = $id;
-        $toot['settings'] = json_decode(TootInstance::find($id)->settings);
+    public function getSettings(Request $request)
+    {   $ti = TootInstance::find($request->get('ti_id'));
+        $toot['id'] = $ti->toot_id;
+        $toot['settings'] = json_decode($ti->settings);
         return view('modules.smsquery.message', compact('toot'));
+    }
+
+    public function getMessage($id, Request $request)
+    {
+        $ti = TootInstance::find($request->get('ti_id'));
+        $toot['id'] = $ti->toot_id;
+        $toot['settings'] = json_decode($ti->settings);
+        $tableName = $ti->toot_id. '_messages';
+        $message = DB::table($tableName)->where('id', $id)->first();
+        return view('modules.smsquery.message', compact('message', 'toot'));
+
     }
 
     public function postMessageCreate(Request $request)
     {
-        $tableName = $request->toot_id . '_messages';
         $message = DB::table($tableName)->insert($request->get('message'));
 
         return $message['id'];
@@ -49,8 +61,22 @@ class RouteController extends Controller {
     {
         $tableName = $request->toot_id . '_replies';
         $reply = DB::table($tableName)->insert($request->get('reply'));
+        $reply = json_decode(json_encode($request->get('reply')), FALSE);
 
-        return $reply['id'];
+        return view('modules.smsquery._reply_tile', compact('reply'));
+
+    }
+
+    public function postLinkMessage(Request $request)
+    {
+
+        $message_id = $request->get('message_id');
+        $ti = $request->get('ti_id');
+
+        $message_id = DB::table($ti . '_messages')->insertGetId($request->get('message'));
+        DB::table($ti . '_replies')->where('id', $request->get('reply_id'))->update(['next_message' => $message_id]);
+
+        return $request->get('message')['name'];
 
     }
 
