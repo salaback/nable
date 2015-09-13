876119bce9ff3c22442a35330740058ce08e4dbb
Sean Alaback <sean.alaback@gmail.com>
2015-09-13 12:07:47 -0400

    Create permissions helper

---
+++ app/Helpers/Permissions.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace app\Helpers;
+
+
+use Illuminate\Support\Facades\Auth;
+
+class Permissions
+{
+    static public function hasPermission($project_id)
+    {
+        $uid = Auth::user()->id;
+        if(DB::table('project_user')->where('user_id', $uid)->where('project_id', $project_id)->first())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
